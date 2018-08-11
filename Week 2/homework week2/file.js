1ef94d8908e7c78ca4706c128188ad4ba3333f13
emmanuelanibeu <emmanuelanibeu1@gmail.com>
2018-08-11 13:15:37 -0400

    -i

---
+++ Week 2/homework week2/file.js
@@ -0,0 +1,38 @@
+var a='Hallo, dunia!';
+var b='Ciao, mondo !';
+var c='Ciao, mondo !';
+console.log(a,b,c);
+
+var j="I'm awesome";
+console.log(j);
+
+var x=
+console.log(10);
+var x ='10';
+console.log(x);
+
+var y ='Toronto,Ontario,Canada';
+console.log(y);
+y.lenght;
+
+var z =7.25;
+console.log(z);
+var t = Math.round(7.25);
+console.log(t);
+
+if (t > z){
+  console.log('t');
+}
+
+var animal= []
+console.log(animal);
+var animal = ["goat","cat","bat"];
+console.log("animal");
+animal.push("kitten");
+console.log("animals")
+
+
+var myString='this ia a test';
+console.log('myString');
+myString.lenght;
+console.log('myString.lenght')
---
+++ Week 5/jsbin file.js
@@ -0,0 +1,35 @@
+
+var a='Hallo, dunia!';
+var b='Ciao, mondo !';
+var c='Ciao, mondo !';
+console.log(a,b,c);
+
+var j="I'm awesome";
+console.log(j);
+
+var x=
+console.log(10);
+var x ='10';
+console.log(x);
+
+var y ='Toronto,Ontario,Canada';
+console.log(y);
+y.lenght;
+
+var z =7.25;
+console.log(z);
+var t = Math.round(7.25);
+console.log(t);
+
+if (t > z){
+  console.log('t');
+}
+
+let animal = ['cat','dog','goat'];
+animal.push('kitten');
+console.log(animal);
+
+
+let myString='this ia a test';
+console.log(myString);
+console.log(myString.lenght);
